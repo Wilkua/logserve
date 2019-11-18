c05a83c15d731c28c642aa34e865275680e72504
William Drescher <wilkua2@gmail.com>
2019-11-18 16:55:43 -0500

    Primary working test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct LogData {
+    level: String,
+    event: String,
+    message: String
 }
+
+fn log(log: web::Json<LogData>) -> impl Responder {
+    println!("[{}] {}: {}", log.level, log.event, log.message);
+    HttpResponse::Accepted()
+}
+
+fn main() {
+    HttpServer::new(|| {
+        App::new()
+            .route("/log", web::post().to(log))
+    })
+    .bind("0.0.0.0:4800")
+    .unwrap()
+    .run()
+    .unwrap();
+}
+
